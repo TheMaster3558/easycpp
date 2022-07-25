06c87da905e364ad54512cb65e4e7ace4ebd8f5a
chawkk6404 <chawk_k@outlook.com>
2022-07-24 17:05:05 -0700

    Add typings

--- easycpp/__init__.py
+++ easycpp/__init__.py
@@ -2,6 +2,8 @@ import cppyy as _cppyy
 import sys as _sys
 import re as _re
 
+from typing import Any, IO, Iterable, List, Set
+
 
 __author__ = 'The Master'
 __name__ = 'easycpp'
@@ -16,7 +18,7 @@ _INCLUDE_REGEX = _re.compile(r'^#include\s*<\w+\.\w+>')
 _OUTSIDE_SUB_REGEX = _re.compile(r'[<>]')
 
 
-def create_cpp_module(name, importable=True):
+def create_cpp_module(name: str, importable: bool = True):
     """
     Easily use C++ in Python with this function.
 
@@ -73,16 +75,15 @@ def create_cpp_module(name, importable=True):
     after = _get_names()
     new_objects = after - before
 
-    module = _CPPModule(name.split('.')[0], new_objects)
+    module = _CPPModule(str(name).split('.')[0], new_objects)
     if importable:
         module.register()
     return module
 
 
-def _include_headers(file):
-    code = file.read()
-
-    matches = _INCLUDE_REGEX.findall(code)
+def _include_headers(file: IO) -> str:
+    code: str = file.read()
+    matches: List[str] = _INCLUDE_REGEX.findall(code)
 
     for match in matches:
         header = _OUTSIDE_SUB_REGEX.sub('', match.strip('#include')).strip()
@@ -92,22 +93,22 @@ def _include_headers(file):
     return code
 
 
-def _get_names():
+def _get_names() -> Set[str]:
     from cppyy import gbl
     return set(dir(gbl))
 
 
 class _CPPModule:
-    def __init__(self, name, names):
+    def __init__(self, name: str, names: Iterable[str]) -> None:
         from cppyy import gbl
 
         self.name = name
         self.names = {n: getattr(gbl, n) for n in names}
 
-    def register(self):
-        _sys.modules[self.name] = self
+    def register(self) -> None:
+        _sys.modules[self.name] = self  # type: ignore
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Any:
         if name in self.names:
             return self.names[name]
-        super().__getattr__(name)
+        super().__getattr__(name)  # type: ignore
